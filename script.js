11400d221284fe7c08758d2530681b9dd8ff349f
Omkar Gujja <67428719+omkarg01@users.noreply.github.com>
2024-06-25 11:13:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const selector = document.getElementById("colorSelect");
+const btn = document.querySelector('input');
+
+btn.addEventListener("click", function(){
+	console.log("Hello");
+	const options = document.querySelector('option');
+	selector.removeChild(options[0])
+});
+
+
+
